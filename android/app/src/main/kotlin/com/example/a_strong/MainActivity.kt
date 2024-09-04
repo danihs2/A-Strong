d5031e58c0e2d3516ba75efd369299eb69ea4eac
Daniel Hachac Salas <daniel.hachac@alumno.buap.mx>
2024-09-04 13:25:29 -0600

    Cambio de versiones fvm

---
+++ android/app/src/main/kotlin/com/example/a_strong/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.a_strong
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:a_strong/app_theme.dart';
+import 'package:a_strong/fitness_app/fitness_app_home_screen.dart';
+import 'package:a_strong/introduction_animation/introduction_animation_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final prefs = await SharedPreferences.getInstance();
+  
+  bool isLogged = prefs.getBool('isLogged') ?? false;
+  runApp(MyApp(
+    home: isLogged ? FitnessAppHomeScreen() : const IntroductionAnimationScreen(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key, this.home});
+  final Widget? home;
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'A Strong',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        textTheme: AppTheme.textTheme,
+        platform: TargetPlatform.iOS,
+      ),
+      home: widget.home,
+    );
+  }
+}
+
+class HexColor extends Color {
+  HexColor(final String hexColor) : super(_getColorFromHex(hexColor));
+
+  static int _getColorFromHex(String hexColor) {
+    hexColor = hexColor.toUpperCase().replaceAll('#', '');
+    if (hexColor.length == 6) {
+      hexColor = 'FF' + hexColor;
+    }
+    return int.parse(hexColor, radix: 16);
+  }
+}
