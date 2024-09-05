99d77bdf8fd06217a5db98f130c77c721f2f6c2d
Daniel Hachac Salas <daniel.hachac@alumno.buap.mx>
2024-09-04 22:17:59 -0600

    noImg

--- back/myapp/models.py
+++ back/myapp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class GrupoMuscular(models.Model):
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField()
-    noImagen = models.IntegerField()
+    noImagen = models.IntegerField(null=True)
 
     def __str__(self):
         return self.nombre
@@ -25,6 +25,7 @@ class Ejercicio(models.Model):
     instrucciones = models.TextField()
     unidad = models.CharField(max_length=4, choices=UNIDAD_OPCIONES)
     tipo_medida = models.CharField(max_length=4, choices=TIPO_MEDIDA_OPCIONES)
+    noImagen = models.IntegerField(null=True)
 
     def __str__(self):
         return self.nombre
